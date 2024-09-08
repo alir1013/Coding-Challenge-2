885b571cb9dadb79335d03d1f34bc1e4ac821ce5
Roan Ali <alir13@usf.edu>
2024-09-08 10:26:23 -0400

    Create and test calculateTip function

--- cc_2.js
+++ cc_2.js
@@ -1,8 +1,18 @@
 //Task 1: Calcualting the Tip
 let bill= 75; //Bill Amount
-let tip= bill >= 300 ? bill * 0.20: bill * 0.15; //calculating tip based on different conditions // Output:11.25
+let tip= bill >= 300 ? bill * 0.20: bill * 0.15; //calculating tip based on different conditions // Output:$11.25
 
 //Task 2: Output Details
 let total= bill + tip;
-console.log(`The bill was $${bill}, the tip was $${11.25}, and total $${total}`) 
+console.log(`The bill was $${bill}, the tip was $${tip}, and total $${total}`);
+
+//Task 3: Creating a function 
+function calculateTip (bill) {return bill 100 ? bill * 0.15: bill * 0.20; // Calculating tip based on the given rule
+}
+let bill= 100;
+let tip= calculateTip (bill);
+let total= bill+tip;
+console.log(`The bill was $${bill}, the tip was $${tip}, and total $${total}`); //Output: $115
+
+
 
